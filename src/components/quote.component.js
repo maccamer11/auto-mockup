3a99d70226a54c3ba55453bcf36daff5ebd021d6
Das <maccameron@live.ca>
2020-02-01 03:04:24 -0700

    finish mockup

--- src/components/quote.component.js
+++ src/components/quote.component.js
@@ -3,11 +3,11 @@ import CustomButtonTwo from '../components/custom-button2.component';
 
 const Quote = () => {
     return (
-        <div className='container'>
-            <div className='row'>
+        <div className='container bg-light'>
+            <div className='row bg-light'>
                 <div className='col-md-6'>
-                    <h3 className='text-center fix'>Receive an answer back with the amount you are guaranteed for all in less than one minute!</h3>
-                    <h4 className='fix text-center'>*Minimum loan of $1000 max $7500*</h4>
+                    <h4 className='text-center fix'>Receive an answer back with the amount you are guaranteed for all in less than one minute!</h4>
+                    <h4 className='fix text-center lead'>*Minimum loan of $1000 max $7500*</h4>
                 </div>
                 <div className='col-md-6 align-self-center'>
                     <CustomButtonTwo />
